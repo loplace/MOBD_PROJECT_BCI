d07b6a4b01f182f588ba0b6e9b2360229748c750
simonedaniello <deliterjob@gmail.com>
2019-06-20 19:18:10 +0200

    dataset splitted

--- load_datasets.R
+++ load_datasets.R
@@ -1,5 +1,6 @@
 # Script to Generate Column Names
 library(dplyr)
+
 load_file_with_names <- function (filePath){
   
   elettrodi <- c("Fz", "Cz", "Pz", "Oz", "P3", "P4", "P7" , "P8")
@@ -10,10 +11,20 @@ load_file_with_names <- function (filePath){
       column_names <- c(column_names,column_name)
       }
   }
+  datasetX_part1 <-read.table("dataset/xaa.txt",col.names = column_names)
+  datasetX_part2 <-read.table("dataset/xab.txt",col.names = column_names)
+  datasetX_part3 <-read.table("dataset/xac.txt",col.names = column_names)
+  datasetX_part4 <-read.table("dataset/xad.txt",col.names = column_names)
   
-  datasetX <-read.table("X.txt",col.names = column_names)
-  datasetC <-read.table("C.txt",col.names = "stimolo")
+  datasetX_1 <- rbind(datasetX_part1, datasetX_part2)
+  datasetX_2 <- rbind(datasetX_part3, datasetX_part4)
+  
+  datasetX <- rbind(datasetX_1, datasetX_2)
+
+  #datasetX <-read.table("X.txt",col.names = column_names)
+  
+  datasetC <-read.table("dataset/C.txt",col.names = "stimolo")
   datasetXC <- cbind(datasetX,cData = datasetC$stimolo)
-  write.csv(datasetXC,"datasetXC.csv")
+  write.csv(datasetXC,"dataset/datasetXC.csv")
 
 }
