38c8ecc97edd671da59b3deca489e37c5f766dfc
Federico <ronci.federico.1993@gmail.com>
2019-07-21 21:33:49 +0200

    added comments in data_preparation

--- functions/data_preparation.R
+++ functions/data_preparation.R
@@ -1,10 +1,23 @@
+#Questi metodi sono atti all'applicazione della media mobile e di un fattore di decimazione ai dataset.
+
+#La media mobile permette di estrapolare il trend di una serie storica, quindi l'idea è che dia risalto
+#alle misurazioni relative al target "1", le quali dovrebbero esibire un trend probabilmente di crescita,
+#a differenza delle misurazione relative al target "-1", che probabilmente mostrano un trend simili tra loro.
+
+#Il fattore di decimazione serve a fare sottocampionamento (Downsampling) per ridurre il numero di variabili in gioco
+#cercando mi mantenere al contempo il carico informativo delle rilevazioni originali per ogni sensore. 
+#Infatti, nonostante le SVM siano meno soggette alla "curse of dimensionality", una svm lineare non è generalmente
+#in grado di separare correttamente punti con un tale numero di attributi, e al contempo usare un kernel che alza troppo 
+#la dimensionalità dello spazio delle features potrebbe portare più facilmente ad overfitting, quindi ridurre il numero
+#di variabili per ogni sensore aumenta notevolemente le prestazioni. 
 
 
 applyMovingAverage <- function(dataset, trainOrTest){
   
+  
   if(trainOrTest == 1){
     trainMA <- applyMovinAverage(dataset,12,"s")
-    colnames(trainMA)[137] <- "target"
+    colnames(trainMA)[137] <- "target" #rinominiamo l'ultima colonna perchè il nome viene sovrascritto durante la apply
     return(trainMA)
   }
   else{
@@ -18,6 +31,8 @@ applyMovingAverage <- function(dataset, trainOrTest){
 
 applyMovinAverage <- function(dataset,backwardFactor,type){
   
+  #utilizziamo la apply per poter effettuare le operazioni (MovAvg, Decimate) per ogni riga del data frame, poi ritrasformiamo
+  #il risultato in un dataframe sfruttando data.frame(matrix(unlist(...)))
   newdataset <- data.frame(matrix(unlist(apply(dataset,1,movingAverageOnSensor,backwardFactor,type))
                                   ,nrow=nrow(dataset),byrow=T  ))
   
@@ -27,6 +42,7 @@ applyMovinAverage <- function(dataset,backwardFactor,type){
 
 movingAverageOnSensor <- function(rowOfDataset,backwardFactor,type){
   
+  #applichiamo la media mobile in maniera separata per ogni sensore (ogni 204 colonne)
   rowMovingAverage1 <- (movavg(as.numeric(rowOfDataset[1:204]) ,backwardFactor,type))
   rowMovingAverage2 <- (movavg(as.numeric(rowOfDataset[205:408]),backwardFactor,type))
   rowMovingAverage3 <- (movavg(as.numeric(rowOfDataset[409:612]),backwardFactor,type))
@@ -36,6 +52,7 @@ movingAverageOnSensor <- function(rowOfDataset,backwardFactor,type){
   rowMovingAverage7 <- (movavg(as.numeric(rowOfDataset[1225:1428]),backwardFactor,type))
   rowMovingAverage8 <- (movavg(as.numeric(rowOfDataset[1429:1632]),backwardFactor,type))
   
+  #applichiamo il fattore di decimazione, trasformiamo in un dataframe ogni porzione di riga e riaggreghiamo
   rowMovingAverage1 <- transpose(as.data.frame(decimate(rowMovingAverage1,backwardFactor)))
   rowMovingAverage2 <- transpose(as.data.frame(decimate(rowMovingAverage2,backwardFactor)))
   rowMovingAverage3 <- transpose(as.data.frame(decimate(rowMovingAverage3,backwardFactor)))
